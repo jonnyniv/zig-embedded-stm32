86518684f59a47e0bd1be8325b3ec16b6e51924e
Jonathan Niven <3747270+jonnyniv@users.noreply.github.com>
2025-07-20 18:09:44 +0100

    Basic code structuring

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,43 @@
 const std = @import("std");
 const regs = @import("registers.zig");
 
-export fn main() noreturn {
+const clk_freq = 8000_000;
+
+fn enable_systick(frequency: usize) void {
+    const counter_val: u24 = @intCast((frequency / 1000) - 1);
+    regs.STK.LOAD_.write(.{ .RELOAD = counter_val });
+    regs.STK.VAL.write(.{ .CURRENT = 0 });
+    regs.STK.CTRL.modify(.{ .ENABLE = 1, .CLKSOURCE = 1 });
+}
+
+fn gpio_init() void {
     // Turn on clock for port C
-    regs.RCC.APB2ENR.modify(.{.IOPCEN = 1,});
+    regs.RCC.APB2ENR.modify(.{ .IOPCEN = 1 });
     // Pin 7 output, open drain
-    regs.GPIOC.CRH.modify(.{.MODE13 = 0b10, .CNF13 = 0b01});
+    regs.GPIOC.CRH.modify(.{ .MODE13 = 0b10, .CNF13 = 0b01 });
+}
+
+fn delay_ms(delay: usize) void {
+    var delay_counter = delay;
+    while (delay_counter > 0) {
+        if (regs.STK.CTRL.read().COUNTFLAG != 0) {
+            delay_counter -= 1;
+        }
+    }
+}
+
+fn init() void {
+    // Configure System clock
+    enable_systick(clk_freq);
+    gpio_init();
+}
+
+export fn main() noreturn {
+    init();
 
     while (true) {
-        for (0..200000) |_| {
-            asm volatile ("nop");
-        }
+        delay_ms(500);
         const val = regs.GPIOC.ODR.read().ODR13;
-        regs.GPIOC.ODR.modify(.{.ODR13 = ~val});
-
+        regs.GPIOC.ODR.modify(.{ .ODR13 = ~val });
     }
 }
